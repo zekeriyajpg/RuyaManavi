b33051ef02f8c2732431e7d7fb0e459704c94ec0
ZEK <ZEK@DESKTOP-MQI8H1N>
2024-07-17 10:27:38 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Transactions;
+
+Console.WriteLine("Elma = 3");
+Console.WriteLine("armut = 4");
+Console.WriteLine("Çilek = 5");
+Console.WriteLine("Diğer ürünler = 3");
+Console.Write("Hangi Meyveyi Almak İstersiniz? (ELma/Armut/Çilek/Diğer) : ");
+string isim = Console.ReadLine();
+
+
+switch (isim)
+{
+    case  "elma" :
+        Console.WriteLine("Seçtiğiniz Meyvenin Fiyatı : 3 TL");
+        break;
+    case "Çilek":
+        Console.WriteLine("Seçtiğiniz Meyvenin Fiyatı : 4 TL");
+        break;
+    case "Armut":
+        Console.WriteLine("Seçtiğiniz Meyvenin Fiyatı : 5 TL");
+        break;
+    default:
+        Console.WriteLine("Seçtiğiniz Meyvenin Fiyatı : 3 TL");
+        break;
+    
+
+}
